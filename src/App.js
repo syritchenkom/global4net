3a810e1573da350aef8bb6b0572ca38deae296de
Mykhailo Syritchenko <syritchenko@gmail.com>
2021-12-01 16:17:57 +0100

    Modified index.js, index.css, App.js and delete reportWebVitals.js, App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+	const [todo, setTodo] = useState('');
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://restcountries.com/v2/name/united')
+			.then((params) => {
+				console.log(params.data);
+			})
+			.then((res) => {
+				setTodos(
+					res.data.map((selectedCountry) => {
+						selectedCountry.completed = false;
+						return selectedCountry;
+					})
+				);
+			});
+	}, []);
+
 	return <div className="App"></div>;
 }
 
