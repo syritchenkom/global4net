7c492402576734acf8e6c562068df26a0be32569
Mykhailo Syritchenko <syritchenko@gmail.com>
2021-12-06 23:43:22 +0100

    Modified src/pages/Home.js and added functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import {
-	Box,
-	Grid,
-	TextField,
-	FormControl,
-	InputLabel,
-	Typography,
-	MenuItem,
-	Select,
-	Container
-} from '@mui/material';
-
-import SearchIcon from '@mui/icons-material/Search';
+import { Grid, Typography, Container } from '@mui/material';
 
 import axios from 'axios';
 
@@ -20,28 +8,30 @@ import Sort from '../components/Sort';
 import '../scss/Pages.scss';
 
 function Home() {
+	const [country, setCountry] = useState('');
 	const [countries, setCountries] = useState([]);
-	const [filredCountries, setFiltredCountries] = useState('');
-	const [string, setString] = useState('');
-
-	const searchChange = (event) => {
-		setFiltredCountries(event.target.value);
-	};
 
 	useEffect(() => {
-		axios.get('https://restcountries.com/v2/all/').then((res) => {
+		axios.get('https://restcountries.com/v3.1/all').then((res) => {
 			setCountries(
 				res.data.map((selectedCountry) => {
-					console.log(selectedCountry);
 					return selectedCountry;
 				})
 			);
 		});
 	}, []);
 
+	if (!countries?.length) {
+		return (
+			<Typography variant="h5" component="h5">
+				Loading...
+			</Typography>
+		);
+	}
+
 	return (
 		<Container className="home">
-			{/* Title */}
+			{/* Title Section */}
 			<Grid container className="homeTitle">
 				<Grid item>
 					<Typography variant="h5" component="h1">
@@ -49,111 +39,13 @@ function Home() {
 					</Typography>
 				</Grid>
 			</Grid>
-
+			{/* Sort and Filtered Section */}
 			<Sort
+				country={country}
+				setCountry={setCountry}
 				countries={countries}
 				setCountries={setCountries}
-				filtred={filredCountries}
-				setFiltred={setFiltredCountries}
 			/>
-
-			{/* Sort Countries
-			<Grid container className="sortContainer">
-				
-				<Grid item className="homeSearch">
-					<Box className="homeSearchBox">
-						<SearchIcon className="homeSearchIcon" />
-						<TextField
-							className="homeSearchText"
-							label="Search country"
-							checked={searchCountries}
-							onChange={searchChange}
-							variant="standard"
-						/>
-					</Box>
-				</Grid>
-
-				
-				<Grid item className="homeFilter">
-					<FormControl variant="standard" className="homeFilterSearch">
-						<InputLabel>Countries...</InputLabel>
-						<Select value={countries} label="Countries">
-							<MenuItem value="">
-								<em>None</em>
-							</MenuItem>
-							<MenuItem value="europe">Europe</MenuItem>
-							<MenuItem value="america">America</MenuItem>
-							<MenuItem value="asia">Asia</MenuItem>
-							<MenuItem value="africa">Africa</MenuItem>
-							<MenuItem value="oceania">Oceania</MenuItem>
-						</Select>
-					</FormControl>
-				</Grid>
-			</Grid> */}
-
-			{/*<Grid item className="">
-					<Stack spacing={2} sx={{ width: 300 }}>
-						<SearchIcon />
-						<Autocomplete
-							freeSolo
-							id="free-solo-2-demo"
-							disableClearable
-							options={countries.map((option) => console.log(option))}
-							renderInput={(params) => (
-								<TextField
-									variant="outlined"
-									{...params}
-									label="Search country"
-									InputProps={{
-										...params.InputProps,
-										type: 'search'
-									}}
-								/>
-							)}
-						/>
-					</Stack>
-				</Grid> */}
-			{/* //////////////////////////////////////////////// */}
-			{/* United country */}
-			{/* <Grid item className="">
-					{countries.map((country) => {
-						const { capital, name, currenciesCode, currenciesName } = country;
-						// console.log(
-						// 	country.currencies.forEach((currencySymbol) =>
-						// 		console.log(currencySymbol.symbol)
-						// 	)
-						// 	 country.currencies.forEach((country) => console.log(country.name))
-						// );
-						return (
-							<article key={currenciesCode}>
-								<div>
-									<h2>
-										Capital: <span>{capital}</span>
-									</h2>
-									<h3>{name}</h3>
-									<h4>
-										Currenciess Name:
-										<span>
-											{country.currencies.forEach((currencyName) =>
-												console.log(currencyName.name)
-											)}
-										</span>
-									</h4>
-									<h4>
-										Currenciess Symbol:
-										<span>
-											{country.currencies.forEach((currencySymbol) =>
-												console.log(currencySymbol.symbol)
-											)}
-										</span>
-									</h4>
-									)}
-								</div>
-							</article>
-						);
-					})}
-				</Grid>
-			</Grid> */}
 		</Container>
 	);
 }
