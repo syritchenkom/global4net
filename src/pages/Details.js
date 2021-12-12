040135afa8e30b876ca6da05bdaa24cc3cb214fc
Mykhailo Syritchenko <syritchenko@gmail.com>
2021-12-12 23:52:55 +0100

    Modified src/pages/Details.js

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,105 +2,71 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 
-import {
-	Container,
-	Card,
-	CardActionArea,
-	CardContent,
-	Grid,
-	Link,
-	Typography
-} from '@mui/material';
+import { Card, CardContent, Grid, Link, Typography } from '@mui/material';
 
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import '../scss/Countries.scss';
+import '../scss/Pages.scss';
 import { useParams } from 'react-router';
 
 const Details = () => {
 	// const [country, setCountry] = useState({});
-	const [country, setCountry] = useState({});
+	const [country, setCountry] = useState();
 	const params = useParams();
 
 	useEffect(() => {
 		axios
-			.get(
-				`https://restcountries.com/v3.1/name/${params.country.toLowerCase()}`
-			)
+			.get(`https://restcountries.com/v3.1/name/${params.name.toLowerCase()}`)
 			.then((res) => {
 				// console.log(res);
 				setCountry(res.data[0]);
 			});
-	}, [params.country]);
+	}, [params.name]);
+	if (!country) {
+		return <div>No details</div>;
+	}
+	const { currencies } = country;
+	const currencyNames = Object.keys(currencies);
 	return (
 		<>
 			<Grid container className="details">
-				<Grid item className="detailsArrow">
-					<Link className="detailsLink" href="/">
-						<ArrowBackIosIcon />
-						Back to Home
-					</Link>
-				</Grid>
-				<Grid item>
-					{console.log(setCountry)}
-					<Card className="countryCard">
-						<CardActionArea className="countryButton">
-							<CardContent className="countryPage">
-								<Typography variant="h5" component="h5">
-									Capital:
-									<span>{country.capital?.[0]}</span>
-									<span>{console.log(country)}</span>
-								</Typography>
-								<Typography variant="h4" component="h4">
-									Name:
-									<span>{country.name?.common}</span>
-								</Typography>
-
-								<Typography variant="h5" component="h5">
-									Currency Code:
-									<span>{country?.currencies?.[0]}</span>
-									<span>{console.log(country.currencies.toString())}</span>
-								</Typography>
-								<Typography variant="h5" component="h5">
-									Currency Name:
-									{/* <span>{state.currency?.name}</span> */}
-								</Typography>
-								{/* <Typography variant="h5" component="h5" gutterBottom>
-									Currency Symbol:
-									<span>{currency?.[currencyCode]?.symbol}</span>
-								</Typography> */}
-							</CardContent>
-						</CardActionArea>
+				<Grid item className="detailsCountry">
+					<Card className="detailsCard countryCard">
+						<div className="detailsLink">
+							<Link className="detailsLinkHome" href="/">
+								<ArrowBackIosIcon />
+								Back to Home
+							</Link>
+						</div>
+						<CardContent className="detailsPage">
+							<Typography variant="h5" component="h5" className="detailsTitle">
+								Capital:
+								<span>{country.capital?.[0]}</span>
+							</Typography>
+							<Typography variant="h4" component="h4">
+								Name:
+								<span>{country.name?.common}</span>
+							</Typography>
+							{currencyNames.map((currency, index) => {
+								return (
+									<div key={index}>
+										<Typography variant="h5" component="h5">
+											Currency Code:
+											<span>{currency}</span>
+										</Typography>
+										<Typography variant="h5" component="h5">
+											Currency Name:
+											<span>{currencies[currency]?.name}</span>
+										</Typography>
+										<Typography variant="h5" component="h5" gutterBottom>
+											Currency Symbol:
+											<span>{currencies[currency]?.symbol}</span>
+										</Typography>
+									</div>
+								);
+							})}
+						</CardContent>
 					</Card>
-
-					{/* <Card className="countryCard">
-						<CardActionArea className="countryButton">
-							<CardContent className="countryPage">
-								<Typography variant="h5" component="h5">
-									Capital:
-									<span>{country.capital?.[0]}</span>
-									<span>{console.log(country)}</span>
-								</Typography>
-								<Typography variant="h4" component="h4">
-									Name:
-									<span>{country.name?.common}</span>
-								</Typography>
-
-								<Typography variant="h5" component="h5">
-									Currency Code:
-									<span>{country.currency}</span>
-									<span>{console.log(country.currencies?.[0])}</span>
-								</Typography>
-								<Typography variant="h5" component="h5">
-									Currency Name:
-									 <span>{state.currency?.name}</span> 
-								</Typography>
-								 <Typography variant="h5" component="h5" gutterBottom>
-									Currency Symbol:
-									<span>{currency?.[currencyCode]?.symbol}</span>
-								</Typography> 
-							</CardContent>
-						</CardActionArea>
-					</Card> */}
 				</Grid>
 			</Grid>
 		</>
